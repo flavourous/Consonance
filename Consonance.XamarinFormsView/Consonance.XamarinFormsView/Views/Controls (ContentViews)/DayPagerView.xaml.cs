31d7a28c3620ff455696a35919617c5c2baf3d98
David <d_higgins@imap.cc>
2015-09-14 09:27:50 +0100

    added diet day control to viewmainy

---
+++ Consonance.XamarinFormsView/Consonance.XamarinFormsView/Views/Controls (ContentViews)/DayPagerView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Consonance.XamarinFormsView
+{
+	public partial class DayPagerView : ContentView
+	{
+		public DayPagerView ()
+		{
+			InitializeComponent ();
+		}
+		DateTime dayWrapper 
+		{
+			get { return (BindingContext as IView).day; }
+			set { (BindingContext as IView).day = value; }
+		}
+		void OnPrev() { dayWrapper = dayWrapper.AddDays (-1); }
+		void OnNext() { dayWrapper = dayWrapper.AddDays (+1); }
+	}
+}
+
